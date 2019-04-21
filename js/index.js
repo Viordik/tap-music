3e2540ed30556ef16816bd85ef5035bb299e29d4
Viordik <fataliev2009@rambler.ru>
2019-04-21 18:23:52 +0300

    Переписан скрипт

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
-  const sounds = document.querySelectorAll('.sound');
+  const soundElements = document.querySelectorAll('.sound');
   const pads = document.querySelectorAll('.pad');
   const visual = document.querySelector('.visual');
-  // const rec = document.querySelector('.rec');
-  // const play = document.querySelector('.play');
-  // const reset = document.querySelector('.reset');
+  const rec = document.querySelector('.rec');
+  const play = document.querySelector('.play');
+  const reset = document.querySelector('.reset');
   const colors = [
     '#60d394',
     '#d36060',
@@ -14,34 +14,116 @@ window.addEventListener('load', () => {
     '#60b2d3'
   ];
 
-  // const arr = [];
+  const makeStore = (state = {}) => {
+    const subscribers = [];
+
+    const subscribe = (fn) => {
+      if (typeof fn === 'function') {
+        subscribers.push(fn);
+      }
+    };
+
+    const setState = (newState = {}) => {
+      state = typeof newState === 'function'
+        ? {...state, ...newState(state)}
+        : {...state, ...newState};
+
+      subscribers.forEach((fn) => {
+        fn(state);
+      });
+    };
+
+    const getState = () => {
+      return state;
+    };
+
+    return {
+      getState,
+      subscribe,
+      setState
+    };
+  };
+
+  const store = makeStore({
+    sounds: [],
+    isRecord: false,
+    isPlay: false
+  });
+
+  store.subscribe((state) => {
+    if (state.isPlay || state.sounds.length === 0) {
+      play.disabled = true
+      play.style.backgroundColor = 'gray';
+    } else {
+      play.disabled = false;
+      play.style.backgroundColor = '#add8e6';
+    }
+  });
 
   //Перебераем список с цветными падами и навешиваем обработчик на каждый пад и функцию воспроизвдения музыки
   pads.forEach((pad, index) => {
     pad.addEventListener('click', function() {
-      sounds[index].currentTime = 0;
-      sounds[index].play();
+      soundElements[index].currentTime = 0; //Запускаем проеигравание музыки с начала, при повторном нажатие на pad или многократном нажатие
+      soundElements[index].play();
 
-      createBubbles(index);
-      // return arr.push(sounds[index]);
+      createBubbles(index); //Создание шарика
+
+      if (store.getState().isRecord) {
+        store.setState((oldState) => {
+          const sounds = [...oldState.sounds, soundElements[index]];
+
+          return { sounds };
+        });
+      }
     });
   });
 
-  // rec.addEventListener('click', function() {
-  //   console.log(arr);
-  // });
+  store.subscribe((state) => {
+      rec.style.backgroundColor = state.isRecord ? 'red' : '#add8e6'; //Если isRecord = true то присваиваем background красный цвет, т.е. при нажатие на кнопку rec background будет красным
+  });
 
-  // play.addEventListener('click', function() {
-  //   arr.forEach((sound, index) => {
-  //     console.log(sound);
-  //     sound.play();
-  //   });
-  // });
+  rec.addEventListener('click', function() {
+    store.setState((oldState) => {
+      return {
+        isRecord: !oldState.isRecord
+      };
+    });
+  });
 
-  // reset.addEventListener('click', function() {
-  //   arr.splice(0, arr.length);
-  //   console.log(arr);
-  // });
+  function playSounds(sounds = []) {
+    if (sounds.length === 0) {
+      store.setState({
+        isPlay: false
+      });
+
+      return;
+    }
+
+    if (!store.getState().isPlay) {
+      store.setState({
+        isPlay: true
+      })
+    }
+
+    const sound = sounds.shift();
+
+    sound.currentTime = 0;
+    sound.play();
+
+    sound.addEventListener('ended', () => {
+      playSounds(sounds);
+    }, { once: true });
+  }
+
+  play.addEventListener('click', function() {
+    playSounds([...store.getState().sounds]);
+  });
+
+  reset.addEventListener('click', function() {
+    store.setState({
+      sounds: []
+    });
+  });
 
   //Функция для создания цветного шарика
   const createBubbles = (index) => {
@@ -53,4 +135,6 @@ window.addEventListener('load', () => {
       visual.removeChild(this);
     });
   };
+
+  store.setState({});
 });
